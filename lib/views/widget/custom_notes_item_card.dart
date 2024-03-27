d21bf45d7a0e8f9fde1c278f2c05b44dbade1ac0
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-03-26 23:18:51 -0700

    display notes part  two

--- lib/views/widget/custom_notes_item_card.dart
+++ lib/views/widget/custom_notes_item_card.dart
@@ -1,35 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:notes/model/note_model.dart';
 
-class NoteItemCard extends StatefulWidget {
+class NoteItemCard extends StatelessWidget {
   const NoteItemCard({super.key, required this.note});
   final NoteModel note;
-  @override
-  State<NoteItemCard> createState() => _NoteItemCardState();
-}
-
-class _NoteItemCardState extends State<NoteItemCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(bottom: 10),
       padding: const EdgeInsets.fromLTRB(16, 24, 0, 16),
       decoration: BoxDecoration(
-        color: Colors.yellow.withOpacity(0.9),
+        color: Color(note.color),
         borderRadius: BorderRadius.circular(12),
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           ListTile(
-            title:  Text(
-              widget.note.title,
-              style: TextStyle(fontSize: 26, color: Colors.black),
+            title: Text(
+              note.title,
+              style: const TextStyle(fontSize: 26, color: Colors.black),
             ),
             subtitle: Padding(
               padding: const EdgeInsets.only(top: 16),
               child: Text(
-                widget.note.subTitle,
+                note.subTitle,
                 style: TextStyle(
                     fontSize: 18, color: Colors.black.withOpacity(0.5)),
               ),
@@ -42,11 +37,11 @@ class _NoteItemCardState extends State<NoteItemCard> {
                   size: 25,
                 )),
           ),
-          const Padding(
-            padding: EdgeInsets.only(right: 24.0),
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
             child: Text(
-              "may 21 2022",
-              style: TextStyle(
+              note.date,
+              style: const TextStyle(
                 fontSize: 15,
                 color: Color.fromARGB(195, 52, 25, 0),
               ),
