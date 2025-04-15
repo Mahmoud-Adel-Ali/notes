cb1c5e2ffc77da3ca437220b10232ab28b6f0076
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-04-15 05:22:26 -0700

    Update Some Versions For Whole App

--- lib/services/custom_local_notificaion_service.dart
+++ lib/services/custom_local_notificaion_service.dart
@@ -2,9 +2,9 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_timezone/flutter_timezone.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
-import 'package:flutter_timezone/flutter_timezone.dart';
 
 abstract class CustomLocalNotificaionService {
   static FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
